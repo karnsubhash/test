218cb25ceaf230d357e45d123873bf44ef999fa3
subhashkarn <subhashkarn>
2024-08-29 13:11:38 +0530

    Data list displayed in tooltip

--- src/route/ImportantPoints/Tooltip.js
+++ src/route/ImportantPoints/Tooltip.js
@@ -24,24 +24,61 @@ const Tooltip = ({ tooltipVisible, tooltipPosition, hoveredData }) => {
           borderRadius: 3,
           padding: 5,
           marginBottom: 3,
+          fontWeight: "bold",
         }}
       >
         {hoveredData?.title}
       </div>
 
-      {!!hoveredData &&
+      {/* {!!hoveredData &&
         hoveredData.linkedTopics.map((i, index) => (
+          <div
+          style={{
+            borderBottom:
+            index !== hoveredData.linkedTopics.length - 1
+            ? "1px solid darkBlue"
+                  : undefined,
+                }}
+                >
+                {i}
+                </div>
+              ))} */}
+      {!!hoveredData &&
+        hoveredData.dataList.map((i, index) => (
           <div
             style={{
               borderBottom:
-                index !== hoveredData.linkedTopics.length - 1
+                index !== hoveredData.dataList.length - 1
                   ? "1px solid darkBlue"
                   : undefined,
+              backgroundColor: index % 2 === 0 ? "lavender" : "lavender",
+              padding: "10px 5px",
+              maxWidth: 500,
             }}
           >
-            {i}
+            <span style={{ fontWeight: "bold" }}>{i.title}</span>&nbsp;-&nbsp;
+            {i.description
+              .split("[BOLD]")
+              .map((d, index) =>
+                index % 2 === 0 ? (
+                  d
+                ) : (
+                  <span style={{ fontWeight: "bold" }}>{d}</span>
+                )
+              )}
           </div>
         ))}
+
+      <div style={{ fontSize: "12px" }}>
+        {!!hoveredData?.linkedTopics?.length ? (
+          <>
+            <span style={{ fontWeight: "bold" }}>Linked Topics</span>
+            &nbsp;-&nbsp;{hoveredData.linkedTopics.join(", ")}
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 };
