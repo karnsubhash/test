17f74ba8819961bb1089f70ee9eb1e487f0ca5fd
subhashkarn <subhashkarn>
2024-09-13 17:06:24 +0530

    Uploaded on internet

--- src/util/axios.js
+++ src/util/axios.js
@@ -3,18 +3,10 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 
 //export const instance = axios.create({baseURL:window.location.origin})
 export const instance = axios.create({
-  baseURL:
-    window.location.protocol + "//" + window.location.hostname + ":" + 8017,
+  baseURL: "https://test-ui-server.vercel.app",
+  // window.location.protocol + "//" + window.location.hostname + ":" + 8017,
 });
 
 export const getImageFromBackend = (imageName) => {
-  return (
-    window.location.protocol +
-    "//" +
-    window.location.hostname +
-    ":" +
-    8017 +
-    "/geographyImages/" +
-    imageName
-  );
+  return "https://test-ui-server.vercel.app/geographyImages/" + imageName;
 };
