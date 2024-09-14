24b11f5aad691f550b95dba4d9cfa7b0501f0434
subhashkarn <subhashkarn>
2024-09-14 11:59:35 +0530

    Code implemented to run on internet and local simultaneously

--- src/util/axios.js
+++ src/util/axios.js
@@ -5,13 +5,13 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 export const instance = axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
-      ? window.location.protocol + "//" + window.location.hostname + ":" + 8016
+      ? window.location.protocol + "//" + window.location.hostname + ":" + 8017
       : "https://test-ui-server.vercel.app",
   // window.location.protocol + "//" + window.location.hostname + ":" + 8017,
 });
 
 export const getImageFromBackend = (imageName) => {
   return process.env.NODE_ENV !== "production"
-    ? "http://localhost:8016/geographyImages/" + imageName
+    ? "http://localhost:8017/geographyImages/" + imageName
     : "https://test-ui-server.vercel.app/geographyImages/" + imageName;
 };
