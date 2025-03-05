b4af55140d2a3da88f18a6b24bc48cd004ed7d05
subhashkarn <subhashkarn>
2025-03-05 11:46:10 +0530

    Economics Ir Page added

--- src/route/Map/index.js
+++ src/route/Map/index.js
@@ -2,10 +2,16 @@ import { Tabs } from "antd";
 import TabPane from "antd/es/tabs/TabPane";
 import React from "react";
 import AncientHistory from "./AncientHistory";
+import EconomicsIR from "./Economics_IR";
 import Geography from "./Geography";
 import MedievalHistory from "./MedievalHistory";
 
-const tabList = ["Geography", "Ancient History", "Medieval History"];
+const tabList = [
+  "Geography",
+  "Ancient History",
+  "Medieval History",
+  "Economics - IR",
+];
 
 const Map = () => {
   const [selectedTab, setSelectedTab] = React.useState("Geography");
@@ -14,10 +20,12 @@ const Map = () => {
     switch (tab) {
       case "Geography":
         return <Geography />;
-        case "Ancient History":
+      case "Ancient History":
         return <AncientHistory />;
-        case "Medieval History":
-        return  <MedievalHistory /> 
+      case "Medieval History":
+        return <MedievalHistory />;
+        case "Economics - IR":
+        return <EconomicsIR />;
       default:
         <div>Under Development</div>;
     }
